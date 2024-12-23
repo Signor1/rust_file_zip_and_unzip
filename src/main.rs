b7d4ff9f318524db5e50ad60b89c6bba8684dd2c
signor1 <emmanuelomemgboji@gmail.com>
2024-12-23 22:36:41 +0100

    feat: added entry point

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,32 @@ fn main() {
     std::process::exit(logic_main());
 }
 
-fn logic_main() -> i32 {}
+fn logic_main() -> i32 {
+    let args: Vec<_> = std::env::args().collect();
+
+    if args.len() < 3 {
+        println!(
+            "Usage: {} <zip|unzip> <filename> [<files_to_zip>...]",
+            args[0]
+        );
+        return 1;
+    };
+
+    let command = &args[1];
+    let filename = &args[2];
+
+    match command.as_str() {
+        "unzip" => unzip_file(filename),
+        "zip" => {
+            if args.len() < 4 {
+                println!("Usage: {} zip <output_zipfile> <files_to_zip>...", args[0]);
+                return 1;
+            }
+            zip_file(filename, &args[3..])
+        }
+        _ => {
+            println!("Invalid command: {}. Use 'zip' or 'unzip'.", command);
+            1
+        }
+    }
+}
